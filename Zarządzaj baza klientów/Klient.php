4735dcf10b4d47942abb9a7dbd124b2533e296ff
lzaglewski <l.zaglewski@gmail.com>
2016-06-07 17:48:01 +0200

    INIT

---
+++ Zarządzaj baza klientów/Klient.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lukasz
+ * Date: 07.06.2016
+ * Time: 17:03
+ */
+
+class Klient {
+    private $_imie;
+    private $_nazwisko;
+    private $_pesel;
+
+    public function __construct($name,$surname,$pes){
+
+        $this->_imie=$name;
+        $this->_nazwisko=$surname;
+        $this->_pesel=$pes;
+
+    }
+
+
+
+    public function getImie(){return $this->_imie;}
+    public function getNazwisko(){ return $this->_nazwisko;}
+    public function getPesel(){return $this->_pesel;}
+
+
+}
---
+++ index.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+
+<?php
+
+require_once("./Zarządzaj baza klientów/Klient.php");
+
+$k=new Klient('lukasz','zaglewski',"900945696096");
+
+
+
+?>
+
+</body>
+</html>
+
+
